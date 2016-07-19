09a11865dc107c995cf0f481734f9e301b10f5a9
jmcreat <jmcreat@naver.com>
2016-07-19 16:03:27 +0900

    cha

--- target/classes/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		C:\Users\dev\.m2\repository\mysql\mysql-connector-java\5.1.31\mysql-connector-java-5.1.31.jar
-	}
-}
-
